e8bf56303e24f012a23ccabfe5c5a364e551eebe
Kevin <contact@kevinpaulidor.de>
2026-03-23 11:05:17 +0100

    refactor layout to conditionally render admin and editor routes

--- src/app/(backoffice)/parallel/layout.tsx
+++ src/app/(backoffice)/parallel/layout.tsx
@@ -5,19 +5,17 @@ import React from 'react'
   faire fonctionner nos routes parrallèles  
   */
 }
-//const getRole = () => Math.random() > 0.5 ? 'admin' : 'editor'
+const getRole = () => Math.random() > 0.5 ? 'admin' : 'editor'
 
 export default async function Layout({children, admin , editor}: {children: React.ReactNode, admin: React.ReactNode, editor: React.ReactNode}) {
+  const role = getRole()
   return (
     <>
       <h2>Gestion Admin / Editor Layout</h2>
       <div className="m-6 flex flex-col">
         {children}
-        {/* 🐶 affiche la route éditor  */}
-        {editor}
-        {/* 🐶 affiche la route admin  */}
-        {admin}
         {/* 🐶 pense à bonus-1. 🚀 rendu conditionel  */}
+        {role === 'admin' && admin}
         {/* 🐶 pense à bonus-2. 🚀 Loading  */}
       </div>
     </>
